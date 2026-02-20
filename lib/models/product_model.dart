9e9190f39c313a598c5eb7d051bd06ce10775721
lazaromane149-lgtm <lazaromane149@gmail.com>
2026-02-20 15:52:45 +0200

    Create Product model with conversion methods
    This class includes methods to convert between a Product object and a Map representation.

---
+++ lib/models/product_model.dart
@@ -0,0 +1,37 @@
+class Product {
+  final String id;
+  final String name;
+  final String description;
+  final double price;
+  final String imageUrl;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+  });
+
+  // Converter de Map para Product
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+      id: map['id'] ?? '',
+      name: map['name'] ?? '',
+      description: map['description'] ?? '',
+      price: map['price']?.toDouble() ?? 0.0,
+      imageUrl: map['imageUrl'] ?? '',
+    );
+  }
+
+  // Converter Product para Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'price': price,
+      'imageUrl': imageUrl,
+    };
+  }
+}
